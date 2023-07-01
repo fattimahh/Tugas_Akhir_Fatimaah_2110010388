64cc7f59882fd0d74ad910fad2fc7d84a1c976fd
yofieedr666 <yofiemadridista@gmail.com>
2023-07-01 17:16:10 +0800

    Membuat Project Dan Unit Data WaliKelas serta button Baru

---
+++ Unit2.pas
@@ -0,0 +1,72 @@
+unit Unit2;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, Grids, DBGrids, frxClass, frxDBSet, DB, ZAbstractRODataset,
+  ZAbstractDataset, ZDataset, ZAbstractConnection, ZConnection, StdCtrls,
+  ExtCtrls;
+
+type
+  TForm2 = class(TForm)
+    Label1: TLabel;
+    Panel1: TPanel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    ComboBox1: TComboBox;
+    Edit3: TEdit;
+    Edit4: TEdit;
+    ComboBox2: TComboBox;
+    Edit5: TEdit;
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    Button5: TButton;
+    Button6: TButton;
+    DataSource1: TDataSource;
+    ZConnection1: TZConnection;
+    ZQuery1: TZQuery;
+    frxDBDataset1: TfrxDBDataset;
+    DBGrid1: TDBGrid;
+    procedure bersih;
+    procedure posisiawal;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm2.Button1Click(Sender: TObject);
+begin
+bersih;
+btn1.Enabled:= False;
+btn2.Enabled:= True;
+btn3.Enabled:= False;
+btn4.Enabled:= False;
+btn5.Enabled:= True;
+btn6.Enabled:= False;
+Edit1.Enabled:= True;
+Edit2.Enabled:= True;
+Edit3.Enabled:= True;
+Edit4.Enabled:= True;
+Edit5.Enabled:= True;
+end;
+
+end.
