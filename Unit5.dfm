920c3122bbd6da6ce011bd5a03bac9582f3a0350
yofieedr666 <yofiemadridista@gmail.com>
2023-07-01 20:02:17 +0800

    Memperbaiki Semua Unit serta Coding Didalamnya

--- Unit5.dfm
+++ Unit5.dfm
@@ -21,8 +21,8 @@ object Form5: TForm5
     Caption = 'DATA CATATAN'
   end
   object DBGrid1: TDBGrid
-    Left = 112
-    Top = 248
+    Left = 16
+    Top = 288
     Width = 320
     Height = 120
     DataSource = DataSource1
@@ -32,11 +32,137 @@ object Form5: TForm5
     TitleFont.Height = -11
     TitleFont.Name = 'MS Sans Serif'
     TitleFont.Style = []
+    OnCellClick = DBGrid1CellClick
+  end
+  object Panel1: TPanel
+    Left = 32
+    Top = 40
+    Width = 673
+    Height = 241
+    TabOrder = 1
+    object Button1: TButton
+      Left = 24
+      Top = 160
+      Width = 75
+      Height = 25
+      Caption = 'BARU'
+      TabOrder = 0
+      OnClick = Button1Click
+    end
+    object Button2: TButton
+      Left = 120
+      Top = 160
+      Width = 75
+      Height = 25
+      Caption = 'SIMPAN'
+      TabOrder = 1
+      OnClick = Button2Click
+    end
+    object Button3: TButton
+      Left = 224
+      Top = 160
+      Width = 75
+      Height = 25
+      Caption = 'UBAH'
+      TabOrder = 2
+      OnClick = Button3Click
+    end
+    object Button4: TButton
+      Left = 336
+      Top = 160
+      Width = 75
+      Height = 25
+      Caption = 'HAPUS'
+      TabOrder = 3
+      OnClick = Button4Click
+    end
+    object Button5: TButton
+      Left = 448
+      Top = 160
+      Width = 75
+      Height = 25
+      Caption = 'BATAL'
+      TabOrder = 4
+      OnClick = Button5Click
+    end
+    object Button6: TButton
+      Left = 552
+      Top = 160
+      Width = 75
+      Height = 25
+      Caption = 'LAPORAN'
+      TabOrder = 5
+    end
+    object Edit1: TEdit
+      Left = 136
+      Top = 40
+      Width = 121
+      Height = 21
+      TabOrder = 6
+      Text = 'Edit1'
+    end
+    object DateTimePicker1: TDateTimePicker
+      Left = 136
+      Top = 8
+      Width = 121
+      Height = 21
+      Date = 45108.813782986110000000
+      Time = 45108.813782986110000000
+      TabOrder = 7
+    end
+    object Edit2: TEdit
+      Left = 136
+      Top = 64
+      Width = 121
+      Height = 21
+      TabOrder = 8
+      Text = 'Edit1'
+    end
+    object Edit3: TEdit
+      Left = 136
+      Top = 96
+      Width = 121
+      Height = 21
+      TabOrder = 9
+      Text = 'Edit1'
+    end
+    object Edit4: TEdit
+      Left = 384
+      Top = 8
+      Width = 121
+      Height = 21
+      TabOrder = 10
+      Text = 'Edit1'
+    end
+    object Edit5: TEdit
+      Left = 384
+      Top = 40
+      Width = 121
+      Height = 21
+      TabOrder = 11
+      Text = 'Edit1'
+    end
+    object Edit6: TEdit
+      Left = 384
+      Top = 64
+      Width = 121
+      Height = 21
+      TabOrder = 12
+      Text = 'Edit1'
+    end
+    object Edit7: TEdit
+      Left = 384
+      Top = 96
+      Width = 121
+      Height = 21
+      TabOrder = 13
+      Text = 'Edit1'
+    end
   end
   object DataSource1: TDataSource
     DataSet = ZQuery1
-    Left = 600
-    Top = 32
+    Left = 592
+    Top = 8
   end
   object ZConnection1: TZConnection
     ControlsCodePage = cGET_ACP
@@ -59,7 +185,7 @@ object Form5: TForm5
       'select * from tabel_catatan')
     Params = <>
     Left = 552
-    Top = 24
+    Top = 8
   end
   object frxReport1: TfrxReport
     Version = '4.0.11'
@@ -76,8 +202,8 @@ object Form5: TForm5
       'begin'
       ''
       'end.')
-    Left = 736
-    Top = 40
+    Left = 672
+    Top = 8
     Datasets = <>
     Variables = <>
     Style = <>
@@ -85,7 +211,7 @@ object Form5: TForm5
   object frxDBDataset1: TfrxDBDataset
     UserName = 'frxDBDataset1'
     CloseDataSource = False
-    Left = 664
-    Top = 48
+    Left = 632
+    Top = 8
   end
 end
--- Unit5.pas
+++ Unit5.pas
@@ -5,7 +5,8 @@ interface
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
   Dialogs, frxClass, frxDBSet, DB, ZAbstractRODataset, ZAbstractDataset,
-  ZDataset, ZAbstractConnection, ZConnection, Grids, DBGrids, StdCtrls;
+  ZDataset, ZAbstractConnection, ZConnection, Grids, DBGrids, StdCtrls,
+  ExtCtrls, ComCtrls;
 
 type
   TForm5 = class(TForm)
@@ -16,6 +17,29 @@ type
     ZQuery1: TZQuery;
     frxReport1: TfrxReport;
     frxDBDataset1: TfrxDBDataset;
+    Panel1: TPanel;
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    Button5: TButton;
+    Button6: TButton;
+    Edit1: TEdit;
+    DateTimePicker1: TDateTimePicker;
+    Edit2: TEdit;
+    Edit3: TEdit;
+    Edit4: TEdit;
+    Edit5: TEdit;
+    Edit6: TEdit;
+    Edit7: TEdit;
+    procedure DBGrid1CellClick(Column: TColumn);
+    procedure Button1Click(Sender: TObject);
+    procedure bersih;
+    procedure posisiawal;
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure Button4Click(Sender: TObject);
+    procedure Button5Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -23,10 +47,177 @@ type
   end;
 
 var
+  id:string;
   Form5: TForm5;
 
 implementation
 
 {$R *.dfm}
+
+procedure TForm5.DBGrid1CellClick(Column: TColumn);
+begin
+id:= zquery1.Fields[0].AsString;
+datetimepicker1.Date:= zquery1.Fields[1].AsDateTime;
+edit1.Text:= zquery1.Fields[2].AsString;
+edit2.Text:= zquery1.Fields[3].AsString;
+edit3.Text:= zquery1.Fields[4].AsString;
+edit4.Text:= zquery1.Fields[5].AsString;
+edit5.Text:= zquery1.Fields[6].AsString;
+edit6.Text:= zquery1.Fields[7].AsString;
+edit7.Text:= zquery1.Fields[8].AsString;
+edit1.Enabled:= True;
+edit2.Enabled:= True;
+edit3.Enabled:= True;
+edit4.Enabled:= True;
+edit5.Enabled:= True;
+edit6.Enabled:= True;
+edit7.Enabled:= True;
+button1.Enabled := False;
+button2.Enabled := False;
+button3.Enabled := True;
+button4.Enabled := True;
+button5.Enabled := True;
+button6.Enabled := False;
+end;
+
+Procedure TForm5.Button1Click(Sender: TObject);
+begin
+bersih;
+button1.Enabled:= false;
+button2.Enabled:= True;
+button3.Enabled:= False;
+button4.Enabled:= False;
+button5.Enabled:= True;
+button6.Enabled:= False;
+Edit1.Enabled:= True;
+Edit2.Enabled:= True;
+Edit3.Enabled:= True;
+Edit4.Enabled:= True;
+Edit5.Enabled:= True;
+Edit6.Enabled:= True;
+Edit7.Enabled:= True;
+end;
+
+procedure TForm5.bersih;
+begin
+edit1.clear;
+edit2.clear;
+edit3.clear;
+edit4.clear;
+edit5.clear;
+edit6.Clear;
+edit7.Clear;
+end;
+
+procedure TForm5.posisiawal;
+begin
+bersih;
+button1.Enabled:= True;
+button2.Enabled:= False;
+button3.Enabled:= False;
+button4.Enabled:= False;
+button5.Enabled:= False;
+button6.Enabled:= True;
+Edit1.Enabled:= False;
+Edit2.Enabled:= False;
+Edit3.Enabled:= False;
+Edit4.Enabled:= False;
+Edit5.Enabled:= False;
+edit6.Enabled:= False;
+Edit7.Enabled:= False;
+end;
+
+procedure TForm5.Button2Click(Sender: TObject);
+begin
+if edit1.Text ='' then
+begin
+ShowMessage('SEMESTER TIDAK BOLEH KOSONG!');
+end else
+if edit2.Text ='' then
+begin
+ShowMessage('ID SISWA TIDAK BOLEH KOSONG!');
+end else
+if edit3.Text ='' then
+begin
+ShowMessage('ID WALI KELAS TIDAK BOLEH KOSONG!');
+end else
+if edit4.Text ='' then
+begin
+ShowMessage('ID ORANG TUA ATAU WALI TIDAK BOLEH KOSONG!');
+end else
+if edit5.Text ='' then
+begin
+ShowMessage('ID POIN TIDAK BOLEH KOSONG!');
+end else
+if edit6.Text ='' then
+begin
+ShowMessage('KETERANGAN TIDAK BOLEH KOSONG!');
+end else
+if edit7.Text ='' then
+begin
+ShowMessage('STATUS TIDAK BOLEH KOSONG!');
+end else
+begin
+zquery1.SQL.Clear;
+zquery1.SQL.Add('insert into tabel_catatan values(null,"'+formatdatetime('yyyy-mm-dd',datetimepicker1.Date)+'","'+edit1.Text+'","'+edit2.Text+'","'+edit3.Text+'","'+edit4.Text+'","'+edit5.Text+'","'+edit6.Text+'","'+edit7.Text+'")');
+zquery1.ExecSQL ;
+zquery1.SQL.Clear;
+zquery1.SQL.Add('select * from tabel_catatan');
+zquery1.Open;
+ShowMessage('Data Berhasil Disimpan');
+posisiawal;
+end;
+end;
+
+procedure TForm5.Button3Click(Sender: TObject);
+begin
+if (edit1.Text= '')or(edit2.Text= '')or(edit3.Text= '')or(edit4.Text= '')or(edit5.Text= '')or(edit6.Text= '')or(edit7.Text= '')then
+begin
+  ShowMessage('Inputan Wajib Di Isi');
+end else
+begin
+zquery1.SQL.Clear;
+zquery1.SQL.Add('update tabel_catatan set tanggal="'+FormatDateTime('yyyy-mm-dd', DateTimePicker1.Date)+'",semester="'+edit1.Text+'",id_siswa="'+edit2.Text+'",id_walikelas="'+edit3.Text+'",id_ortuatauwali="'+edit4.Text+'",id_point="'+edit5.Text+'",keterangan="'+edit6.Text+'",status="'+edit7.Text+'" where id_catatan ="'+id+'"');
+zquery1.ExecSQL;
+zquery1.SQL.Clear;
+zquery1.SQL.Add('select * from tabel_catatan');
+zquery1.Open;
+ShowMessage('Data Berhasil Disimpan');
+posisiawal;
+
+end;
+end;
+
+procedure TForm5.Button4Click(Sender: TObject);
+begin
+if MessageDlg('APAKAH YAKIN MENGHAPUS DATA INI?', mtWarning, [mbYes, mbNo], 0) = mrYes then
+begin
+  zquery1.SQL.Clear;
+  zquery1.SQL.Add('delete from tabel_catatan where id_catatan ="'+id+'"');
+  zquery1.ExecSQL;
+  zquery1.SQL.Clear;
+  zquery1.SQL.Add('select * from tabel_catatan');
+  zquery1.Open;
+  ShowMessage('DATA BERHASIL DIHAPUS');
+  posisiawal;
+end
+else
+begin
+  ShowMessage('DATA BATAL DIHAPUS');
+  posisiawal;
+end;
+end;
+
+procedure TForm5.Button5Click(Sender: TObject);
+begin
+Edit1.Clear;
+Edit2.Clear;
+Edit3.Clear;
+Edit4.Clear;
+Edit5.Clear;
+Edit6.Clear;
+Edit7.Clear;
+posisiawal;
+end;
 
 end.
